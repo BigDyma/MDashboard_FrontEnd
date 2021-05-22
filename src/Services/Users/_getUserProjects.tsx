412912598e8f1f8dc906a87f868a606c13f2c08b
bigdyma <d1mastrelet@gmail.com>
2021-05-22 20:48:07 +0300

    last commit update

--- src/Services/Users/_getUserProjects.tsx
+++ src/Services/Users/_getUserProjects.tsx
@@ -2,7 +2,7 @@ import api from '../axios-config'
 import {throwIfError} from '../Helpers/throwCustomException';
 
 
-const getUser = async (id:number) => {
+const getUserProjects = async (id:number) => {
     const result = await api().get(`/Users/${id}/projects`)
     throwIfError(result);
 
---
+++ src/Services/Users/_getUserReports.tsx
@@ -0,0 +1,10 @@
+import api from '../axios-config'
+import {throwIfError} from '../Helpers/throwCustomException';
+
+
+const getUserReports = async (id:number) => {
+    const result = await api().get(`/Users/${id}/reports`)
+    throwIfError(result);
+
+    return result;
+}
